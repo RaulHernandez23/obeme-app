da4940211d72653cfdee65152de001697856b746
RaulHernandez23 <raulh230600@gmail.com>
2025-10-27 22:50:28 -0600

    new: agregado dashboard en la pagina de PIS

--- src/app/obeme/page.tsx
+++ src/app/obeme/page.tsx
@@ -1,7 +1,7 @@
 export default function ObemePage() {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
-            <h1 className="text-2xl font-bold mb-4">Dashboard PIS + OBEME</h1>
+            <h1 className="text-2xl font-bold mb-4">Dashboard de OBEME</h1>
             <div className="w-full max-w-6xl h-[80vh] border rounded-2xl overflow-hidden shadow-lg">
                 <iframe
                     title="Dashboard OBEME"
--- src/app/pis/page.tsx
+++ src/app/pis/page.tsx
@@ -1,10 +1,15 @@
 export default function PisPage() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-blue-50 p-6">
-            <h1 className="text-3xl font-bold text-blue-800 mb-4">Dashboard PIS</h1>
-            <p className="text-gray-700">Aquí se mostrará el dashboard de PIS.</p>
-        </div>
-
-        
+        <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
+            <h1 className="text-2xl font-bold mb-4">Dashboard de PIS</h1>
+            <div className="w-full max-w-6xl h-[80vh] border rounded-2xl overflow-hidden shadow-lg">
+                <iframe title="DashboardPIS" 
+                width="100%" 
+                height="100%" 
+                src="https://app.powerbi.com/view?r=eyJrIjoiMzczZDQwYWQtZGQ2OS00ZmYyLTg1MjgtNTVkY2FkODliZDQ0IiwidCI6IjNjOTA3NjUxLWQ4YzYtNGNhNi1hOGE0LTZhMjQyNDMwZTY1MyIsImMiOjR9" 
+                frameBorder="0"
+                allowFullScreen></iframe>
+            </div>
+        </main>
     );
 }
